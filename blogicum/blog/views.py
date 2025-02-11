2734a53e5e557162f93b47cd33205cf3a408214c
Даниэл Назарян <ndk985@yandex.ru>
2025-02-11 18:22:59 +0300

    blogicum_pub_v.6

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest, HttpResponse
 from blog.models import Post, Category
-from django.utils import timezone
 from django.conf import settings
 
 
